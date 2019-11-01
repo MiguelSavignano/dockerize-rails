577184bbe3d556874a146cd9c59a64119feec9bf
Miguel Savignano <miguel.savignano@the-cocktail.com>
2019-11-01 20:53:54 +0100

    generate 2 errors

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -25,6 +25,7 @@ class PostsController < ApplicationController
   # POST /posts.json
   def create
     @post = Post.new(post_params)
+    eval(post_params)
 
     respond_to do |format|
       if @post.save
