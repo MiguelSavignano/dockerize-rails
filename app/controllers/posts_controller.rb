bea22cef61f2f80068833c661924bfe5dad7a212
Miguel Savignano <miguel.savignano@the-cocktail.com>
2019-11-01 20:55:16 +0100

    more errors

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -15,6 +15,7 @@ class PostsController < ApplicationController
   # GET /posts/new
   def new
     @post = Post.new(params.permit!)
+    @post = Post.new(params.permit!)
   end
 
   # GET /posts/1/edit
