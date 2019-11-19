9b0292301f8f6bd94c180396a0b7e830043416be
miguemasx <migue.masx@gmail.com>
2019-11-19 19:24:10 +0100

    fix

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -3,7 +3,7 @@ Rails.application.configure do
 
   # Code is not reloaded between requests.
   config.cache_classes = true
-  config.logging.project_id.log_name = 'rails'
+  config.logging.log_name = 'rails'
   # Eager load code on boot. This eager loads most of Rails and
   # your application in memory, allowing both threaded web servers
   # and those relying on copy on write to perform better.
