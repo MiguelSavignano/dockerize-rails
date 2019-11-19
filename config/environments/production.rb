0ee85a92fd61477fd1075aed841cb4b11a6cc3b6
miguemasx <migue.masx@gmail.com>
2019-11-19 19:21:15 +0100

    set log name configuration

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -3,7 +3,7 @@ Rails.application.configure do
 
   # Code is not reloaded between requests.
   config.cache_classes = true
-
+  config.logging.project_id.log_name = 'rails'
   # Eager load code on boot. This eager loads most of Rails and
   # your application in memory, allowing both threaded web servers
   # and those relying on copy on write to perform better.
@@ -84,7 +84,6 @@ Rails.application.configure do
     logger           = ActiveSupport::Logger.new(STDOUT)
     logger.formatter = config.log_formatter
     config.logger    = ActiveSupport::TaggedLogging.new(logger)
-    Rails.logger.progname = "rails" if Rails.logger.respond_to?('progname')
   end
 
   # Do not dump schema after migrations.
