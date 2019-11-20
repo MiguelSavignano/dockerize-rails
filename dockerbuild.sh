eb8c3c8c759a13d966b21c125142cc8e4251e1bc
miguemasx <migue.masx@gmail.com>
2019-11-20 11:28:27 +0100

    .

--- dockerbuild.sh
+++ dockerbuild.sh
@@ -1,6 +1,7 @@
 set -e
 
 IMAGE_NAME=devmasx/rails-sqlite
+SERVICE_NAME=rails-nginx
 GIT_HASH=`git rev-parse --short HEAD`
 
 docker build -t rails-sqlite -t $IMAGE_NAME -t $IMAGE_NAME:$GIT_HASH .
@@ -11,5 +12,5 @@ docker push $IMAGE_NAME:$GIT_HASH
 echo  $IMAGE_NAME:$GIT_HASH
 
 if [ "$1" == "--deploy" ]; then
-  kubectl set image deployment/rails-nginx rails=$IMAGE_NAME:$GIT_HASH
+  kubectl set image deployment/$SERVICE_NAME rails=$IMAGE_NAME:$GIT_HASH
 fi
